f3c4cdc1c6947ce48dc0b32e8f3bff78af39e848
Nahid51 <hasannahidnahid@gmail.com>
2022-08-19 23:03:29 +0600

    create env file for security

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import morgan from "morgan";
 import 'dotenv/config';
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 import userRouter from "./routes/user.js";
 import foodRouter from "./routes/food.js";
@@ -18,10 +18,10 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use("/users", userRouter);
 app.use("/foods", foodRouter);
 
-const mongodbURL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.pf4kpjf.mongodb.net/food_fiesta?retryWrites=true&w=majority`;
+const databaseURL = `${process.env.DB_INFO}`;
 
 
-mongoose.connect(mongodbURL)
+mongoose.connect(databaseURL)
     .then(() => {
         app.listen(port, () => console.log(`Server running on port ${port}`));
     })
